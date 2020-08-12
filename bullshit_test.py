ddc5cb67f0b37dcbe72c29764200f40eca8b3c2d
Mac-Nikak <65832228+Mac-Nikak@users.noreply.github.com>
2020-08-12 11:14:22 +0700

    Update bullshit_test.py

--- bullshit_test.py
+++ bullshit_test.py
@@ -99,7 +99,7 @@ async def main(source: str, goal: str):
                 print('Result found!')
                 break
 
-source = 'https://ru.wikipedia.org/wiki/%D0%A2%D0%BE%D1%87%D0%BA%D0%B0_(%D0%B3%D0%B5%D0%BE%D0%BC%D0%B5%D1%82%D1%80%D0%B8%D1%8F)'
-goal = 'https://ru.wikipedia.org/wiki/%D0%98%D1%81%D0%BB%D0%B0%D0%BC%D1%81%D0%BA%D0%B0%D1%8F_%D1%84%D0%B8%D0%BB%D0%BE%D1%81%D0%BE%D1%84%D0%B8%D1%8F'
+source = 'https://ru.wikipedia.org/wiki/%D0%92%D0%BE%D1%80%D0%BE%D0%BD'
+goal = 'https://ru.wikipedia.org/wiki/%D0%9F%D0%B8%D1%81%D1%8C%D0%BC%D0%B5%D0%BD%D0%BD%D1%8B%D0%B9_%D1%81%D1%82%D0%BE%D0%BB'
 
 asyncio.run(main(source, goal))
