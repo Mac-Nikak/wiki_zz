8811b23949cd9ece09b9779436b62576c828edcb
Mac-Nikak <65832228+Mac-Nikak@users.noreply.github.com>
2020-08-12 11:08:11 +0700

    Update bullshit_test.py

--- bullshit_test.py
+++ bullshit_test.py
@@ -100,6 +100,6 @@ async def main(source: str, goal: str):
                 break
 
 source = 'https://ru.wikipedia.org/wiki/%D0%9F%D1%80%D0%BE%D1%81%D1%82%D1%80%D0%B0%D0%BD%D1%81%D1%82%D0%B2%D0%BE_(%D0%BC%D0%B0%D1%82%D0%B5%D0%BC%D0%B0%D1%82%D0%B8%D0%BA%D0%B0)'
-goal = 'https://ru.wikipedia.org/wiki/%D0%9F%D1%80%D0%BE%D1%81%D1%82%D1%80%D0%B0%D0%BD%D1%81%D1%82%D0%B2%D0%BE_%D0%B2_%D1%84%D0%B8%D0%B7%D0%B8%D0%BA%D0%B5'
+goal = 'https://ru.wikipedia.org/wiki/%D0%9B%D0%B8%D0%BD%D0%B5%D0%B9%D0%BD%D0%B0%D1%8F_%D1%84%D0%BE%D1%80%D0%BC%D0%B0'
 
 asyncio.run(main(source, goal))
